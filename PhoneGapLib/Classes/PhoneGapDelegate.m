1190031e0a796d4e9674bb2473aef3bf03e366a4
Jason Tokoph <jtokoph@mozes-inc.com>
2011-01-27 16:16:05 -0800

    Fixed jumping loading image

--- PhoneGapLib/Classes/PhoneGapDelegate.m
+++ PhoneGapLib/Classes/PhoneGapDelegate.m
@@ -250,12 +250,12 @@ static NSString *gapVersion;
 	 */
 	UIImage* image = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Default" ofType:@"png"]];
 	imageView = [[UIImageView alloc] initWithImage:image];
-	[imageView setFrame:window.bounds];
+	[imageView setFrame:screenBounds];
 	[imageView setContentMode:UIViewContentModeScaleAspectFill];
 	[image release];
 	
     imageView.tag = 1;
-	[window addSubview:imageView];
+	[viewController.view addSubview:imageView];
 	[imageView release];
 
 	/*
